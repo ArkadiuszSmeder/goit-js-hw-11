24274e691f473afe11e7ca8fbfbf3fe3ea29ddc7
ArkadiuszSmeder <arkadiusz.smeder@gmail.com>
2024-03-02 08:49:43 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import Notiflix from 'notiflix';
 import axios from 'axios';
 
 const apiKey = "42573503-814be0cbf75c4ae20afa280cd";
+const searchQuery = document.querySelector('.search-form').elements.searchQuery.value;
+let currentPage = 1; // Zmienna do przechowywania bieżącej strony wyników
 
 // Funkcja do wysyłania zapytania do API Pixabay i tworzenia galerii
 async function fetchImagesAndGallery(searchQuery, apiKey) {
   const gallery = document.querySelector('.gallery');
   
-  gallery.innerHTML = '';
+  if (page === 1) {
+    gallery.innerHTML = ''; // Czyszczenie galerii tylko przy pierwszej stronie wyników
+  }
 
   const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
   
@@ -32,6 +36,14 @@ async function fetchImagesAndGallery(searchQuery, apiKey) {
       `).join('');
 
       gallery.innerHTML = imagesHTML;
+
+      // Wyświetlenie przycisku "Load more" tylko wtedy, gdy jest dostępne więcej wyników
+      if (data.totalHits > page * 40) {
+        loadMoreButton.style.display = 'block';
+      } else {
+        loadMoreButton.style.display = 'none';
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+      }
     }
   } catch (error) {
     console.error('Error fetching images:', error);
@@ -41,11 +53,17 @@ async function fetchImagesAndGallery(searchQuery, apiKey) {
 };
 
 
-//wywołanie funkcji
+// Wywołanie funkcji przy przesłaniu formularza wyszukiwania
 const searchForm = document.querySelector('.search-form');
 searchForm.addEventListener('submit', function(event) {
   event.preventDefault();
-
-  const searchQuery = this.elements.searchQuery.value;
+  currentPage = 1; // Resetowanie strony do 1 po wysłaniu nowego zapytania
   fetchImagesAndGallery(searchQuery, apiKey);
+});
+
+// Wywołanie funkcji przy kliknięciu przycisku "Load more"
+const loadMoreButton = document.querySelector('.load-more');
+loadMoreButton.addEventListener('click', function() {
+  currentPage++;
+  fetchImagesAndGallery(searchQuery, apiKey, currentPage);
 });
